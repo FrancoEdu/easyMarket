c4a113cfa612707128946e786f7240e5e0ff3fd8
Eduardo <eduardo@comandago.com.br>
2025-01-21 13:46:40 -0300

    input and header components ready to be used in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { ButtonComponent } from "../../components/button/button.component";
+import { InputComponent } from "../../components/input/input.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent, ButtonComponent, InputComponent],
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
