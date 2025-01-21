9ed50aa6a377936676752d2c2c4a7a18ab4b49fb
Eduardo <eduardo@comandago.com.br>
2025-01-21 18:04:33 -0300

    navigation to new product and cart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,13 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { ButtonComponent } from "../../components/button/button.component";
 import { InputComponent } from "../../components/input/input.component";
 import { FlatlistComponent } from "../../components/flatlist/flatlist.component";
+import { Router } from '@angular/router';
+import { RoutesPath } from '../../app.routes';
+
+enum navigationPossibilities {
+  CART = RoutesPath.CART,
+  NEW_PRODUCT = RoutesPath.NEW_PRODUCT
+}
 
 @Component({
   selector: 'app-home',
@@ -12,4 +19,13 @@ import { FlatlistComponent } from "../../components/flatlist/flatlist.component"
 })
 export class HomeComponent {
 
+  navigationOptionsPossibilities = navigationPossibilities;
+
+  constructor(
+    private readonly _router: Router,
+  ) { }
+  
+  handleClickButtonNavigation(navigateTo: navigationPossibilities): void {
+    this._router.navigate([navigateTo]);
+  }
 }
