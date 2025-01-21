e49f6edb46eb30b10f639fd5bbca3e3362677d49
Eduardo <eduardo@comandago.com.br>
2025-01-21 10:52:15 -0300

    Entry screen ready

--- src/app/pages/entry/entry.component.ts
+++ src/app/pages/entry/entry.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { ImageComponent } from '../../components/image/image.component';
+import { SubTitleComponent } from '../../components/sub-title/sub-title.component';
+import { TitleComponent } from '../../components/title/title.component';
+import { Router } from '@angular/router';
+import { RoutesPath } from '../../app.routes';
 
 @Component({
   selector: 'app-entry',
-  imports: [],
+  imports: [TitleComponent, SubTitleComponent, ButtonComponent, ImageComponent],
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.scss']
 })
 export class EntryComponent {
+
+  constructor(
+    private readonly _router: Router,
+  ) {}
+
+  handleClickGoHome(): void {
+    this._router.navigate([RoutesPath.HOME]);
+  }
 }
