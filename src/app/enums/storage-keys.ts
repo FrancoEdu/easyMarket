b597f71fb0d5a11064b637ad7b20abd0b4094fba
Eduardo <eduardo@comandago.com.br>
2025-01-25 15:40:13 -0300

    add product service for managing products in local storage

---
+++ src/app/enums/storage-keys.ts
@@ -0,0 +1,4 @@
+export enum StorageKeys {
+  PRODUCTS = '_products',
+  FINAL_PRICE = '_final_price',
+}
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
+import { StorageKeys } from '../enums/storage-keys';
+import { StorageService } from './storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private readonly _localStorage: StorageService) { }
+
+  /**
+   * Obtém todos os produtos armazenados.
+   * @returns Lista de produtos.
+   */
+  getAllProducts(): Product[] {
+    return this._localStorage.getItem<Product[]>(StorageKeys.PRODUCTS) ?? [];
+  }
+
+  /**
+   * Obtém um produto pelo ID.
+   * @param id ID do produto.
+   * @returns Produto encontrado ou null.
+   */
+  getProductById(id: string): Product | null {
+    const products = this.getAllProducts();
+    return products.find(product => product.Id === id) || null;
+  }
+
+  /**
+   * Adiciona um novo produto ao armazenamento.
+   * @param product Produto a ser adicionado.
+   */
+  addProduct(product: Product): void {
+    const products = this.getAllProducts();
+    products.push(product);
+    this.saveProducts(products);
+  }
+
+  /**
+   * Atualiza um produto existente.
+   * @param product Produto atualizado.
+   */
+  updateProduct(product: Product): void {
+    const products = this.getAllProducts();
+    const index = products.findIndex(p => p.Id === product.Id);
+
+    if (index !== -1) {
+      products[index] = product;
+      this.saveProducts(products);
+    } else {
+      console.error(`Produto com ID ${product.Id} não encontrado para atualização.`);
+    }
+  }
+
+  /**
+   * Remove um produto pelo ID.
+   * @param id ID do produto a ser removido.
+   */
+  removeProduct(id: string): void {
+    let products = this.getAllProducts();
+    products = products.filter(product => product.Id !== id);
+    this.saveProducts(products);
+  }
+
+  /**
+   * Limpa todos os produtos armazenados.
+   */
+  clearProducts(): void {
+    this._localStorage.removeItem(StorageKeys.PRODUCTS);
+  }
+
+  /**
+   * Pesquisa produtos pelo texto.
+   * @param text Texto a ser pesquisado.
+   */
+  searchBytext(text: string): Product[] {
+    const products = this.getAllProducts();
+    return products.filter(product => product.Name.toLowerCase().includes(text.toLowerCase()));
+  }
+
+  /**
+   * Salva a lista de produtos no this._localStorage.
+   * @param products Lista de produtos a ser salva.
+   */
+  private saveProducts(products: Product[]): void {
+    this._localStorage.setItem(StorageKeys.PRODUCTS, JSON.stringify(products));
+  }
+}
