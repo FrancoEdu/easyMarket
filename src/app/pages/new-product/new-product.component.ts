8508567cbb32e2f1a891fc9133781cef0e66c639
Eduardo <eduardo@comandago.com.br>
2025-01-26 17:41:08 -0300

    feat: implement reactive form for new product with validation and conditional fields

--- src/app/pages/new-product/new-product.component.ts
+++ src/app/pages/new-product/new-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputComponent } from '../../components/input/input.component';
 import { ButtonComponent } from '../../components/button/button.component';
 import { HeaderComponent } from "../../components/header/header.component";
@@ -8,29 +9,49 @@ import { _productsThatUsePerKilo, _productsThatUseQuantity, CategoryOfProducts }
 
 @Component({
   selector: 'app-new-product',
-  imports: [HeaderTitleComponent, InputComponent, CategoryDropdownComponent, ButtonComponent, HeaderComponent],
+  imports: [HeaderTitleComponent, InputComponent, CategoryDropdownComponent, ButtonComponent, HeaderComponent, ReactiveFormsModule],
   templateUrl: './new-product.component.html',
   styleUrls: ['./new-product.component.scss']
 })
 export class NewProductComponent {
 
   optionSelected?: CategoryOfProducts;
+  productForm!: FormGroup; 
+
+  constructor(
+    private readonly _formBuilder: FormBuilder,
+  ) {
+    this.buildResourceForm();
+  }
 
   handleOptionSelected(event: string): void {
     this.optionSelected = CategoryOfProducts[event as keyof typeof CategoryOfProducts];
   }
 
   handleClickButtonSave(): void {
-
+    console.log('Formulário válido. Dados:', this.productForm.value);
   }
 
+  
   get isProductThatUsePerKilo(): boolean {
     if (this.optionSelected === undefined || this.optionSelected === null) return false;
     return _productsThatUsePerKilo.includes(this.optionSelected);
   }
-
+  
   get isProductThatUsePerUnit(): boolean {
     if (this.optionSelected === undefined || this.optionSelected === null) return false;
     return _productsThatUseQuantity.includes(this.optionSelected);
+  }
+
+  private buildResourceForm(): void {
+    this.productForm = this._formBuilder.group({
+      name: ['', Validators.required], // Nome do produto (obrigatório)
+      category: [null, Validators.required], // Categoria do produto (obrigatório)
+      pricePerKilo: [null], // Preço por quilo (opcional)
+      kilo: [null], // Peso em quilos (opcional)
+      unitOrBale: [true], // Unidade ou Fardo (padrão: Unidade)
+      unitPrice: [null], // Preço por unidade (opcional)
+      quantity: [null], // Quantidade (opcional)
+    });
   }
 }
