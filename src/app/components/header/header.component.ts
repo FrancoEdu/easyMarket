3e1c2c87cb36ed23dfa02df56b4407c94898ee23
Eduardo <eduardo@comandago.com.br>
2025-01-26 17:14:35 -0300

    feat: enhance header component with dynamic text and background color support

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  spanText = input<string>('Sua compra já está em');
+  price = input<number>(0.0);
 
+  backgroundColor = input<string>('background-green');
 }
--- src/app/pages/new-product/new-product.component.ts
+++ src/app/pages/new-product/new-product.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../../components/input/input.component';
+import { ButtonComponent } from '../../components/button/button.component';
+import { HeaderComponent } from "../../components/header/header.component";
 import { HeaderTitleComponent } from '../../components/header-title/header-title.component';
 import { CategoryDropdownComponent } from '../../components/category-dropdown/category-dropdown.component';
 import { _productsThatUsePerKilo, _productsThatUseQuantity, CategoryOfProducts } from '../../enums/category-of-food.enum';
 
 @Component({
   selector: 'app-new-product',
-  imports: [HeaderTitleComponent, InputComponent, CategoryDropdownComponent],
+  imports: [HeaderTitleComponent, InputComponent, CategoryDropdownComponent, ButtonComponent, HeaderComponent],
   templateUrl: './new-product.component.html',
   styleUrls: ['./new-product.component.scss']
 })
@@ -16,7 +18,10 @@ export class NewProductComponent {
 
   handleOptionSelected(event: string): void {
     this.optionSelected = CategoryOfProducts[event as keyof typeof CategoryOfProducts];
-    console.log('Option selected:', this.optionSelected);
+  }
+
+  handleClickButtonSave(): void {
+
   }
 
   get isProductThatUsePerKilo(): boolean {
