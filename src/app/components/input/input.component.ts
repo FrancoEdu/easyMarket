9ad428b88abb93197e8195aea6d759322688c923
Eduardo <eduardo@comandago.com.br>
2025-01-27 20:32:20 -0300

    feat: implement ControlValueAccessor for input component and update template for two-way binding

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,20 +1,46 @@
-import { Component, input, output } from '@angular/core';
+import { Component, forwardRef, input, output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   imports: [],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+  styleUrl: './input.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   placeholder = input<string>("Pesquise pelo produto...");
   valueTextInput = output<string>();
 
   inputValue: string = '';
+  onChange: any = () => {};
+  onTouched: any = () => {};
 
   onInputChange(event: Event): void {
     const target = event.target as HTMLInputElement;
     this.inputValue = target.value;
+    this.onChange(this.inputValue); // Notifica o Angular sobre a mudança
     this.valueTextInput.emit(this.inputValue);
+  }
+
+  // Método para escrever o valor no componente
+  writeValue(value: any): void {
+    this.inputValue = value;
+  }
+
+  // Método para registrar a função de mudança
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  // Método para registrar a função de "touched"
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
   }
 }
